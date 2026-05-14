970fcb25b8cf247ec80580ef019927b2aacac405
Komissar <komissar@MacBook-Pro-Komissar.local>
2026-05-15 01:59:58 +0300

    fix: bug

--- backend/src/follows/follows.module.ts
+++ backend/src/follows/follows.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { FollowsController } from './follows.controller';
 import { FollowsService } from './follows.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [FollowsController],
   providers: [FollowsService],
 })
